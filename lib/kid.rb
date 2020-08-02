14f2cb3d8d9cbc8d5ea054b37375afd346670c99
Ebo Lee <yiboli.coding@gmail.com>
2020-08-02 17:20:42 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,5 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 
 class Kid
   include Dance
@@ -11,5 +12,5 @@ class Kid
   end
 
   extend MetaDancing
-  
+
 end
